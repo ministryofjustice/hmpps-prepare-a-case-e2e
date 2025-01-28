64b86e81305abffa9b4741d2499a54ff1b10ec8e
Daniel Lackenby <11579453+KiraNamida@users.noreply.github.com>
2025-01-28 10:13:41 +0000

    Tech Work: e2e structure work (#12)
    * Restructure to project to include project dependencies and prep utils as a source of truth for setting up date
    
    * Ensure all references to our test utils use the global @

---
+++ utils/caseHearingUtils.ts
@@ -0,0 +1,26 @@
+import { APIRequestContext, expect } from '@playwright/test';
+import { getTestConfig } from './config/testConfig';
+import { generateRandomHearingId } from './generate-random-hearing-id'
+import { replaceJsonProperty } from './replace-json-property'
+
+export const generateHearing = async (request: APIRequestContext, hearing: any) => {
+    const config = getTestConfig();
+    const id = generateRandomHearingId()
+    const updatedonehearing = replaceJsonProperty(JSON.stringify(hearing), `${id}`);
+
+    const firstName = hearing.hearing.prosecutionCases[0].defendants[0].personDefendant.personDetails.firstName;
+    const lastName = hearing.hearing.prosecutionCases[0].defendants[0].personDefendant.personDetails.lastName;
+    const fullName = firstName + ' ' + lastName;
+
+    const response = await request.post(`${config.services.courtHearing.urls.eventReceiver}` + '/hearing/' + `${id}`, {
+      headers: { "Authorization": `Bearer ${config.auth.token}`, },
+      data: JSON.parse(updatedonehearing)
+    })
+    expect(response.status()).toBe(200)
+
+    return fullName
+}
+
+export default {
+  generateHearing
+}
